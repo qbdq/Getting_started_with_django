9b726f139ab2a8f9bd1d9249f262f59b51cfe4e1
qbdq <abdo@DESKTOP-6I6CEEG.localdomain>
2021-10-10 22:46:42 +0000

    Test commit

---
+++ apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class GettingStartedWithDjangoConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'getting_started_with_django'
